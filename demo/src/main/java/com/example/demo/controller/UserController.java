f1563519fd1e64b6cc3613268d4e08cfcdee6343
sdsingh2000 <boomboombassdjsd@gmail.com>
2022-12-08 21:43:32 +0530

    ---

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("hgffgfdxd");
+    }
+}
